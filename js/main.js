03c92a6788e53a4f0337d2f9d0c94ddeada9d6ab
adityasundaram <don.aditya@gmail.com>
2014-11-10 21:33:04 +0530

    my first commit

--- js/main.js
+++ js/main.js
@@ -204,6 +204,7 @@ function requestTurn(turn_url) {
     console.log('Getting TURN server from ', turn_url);
     // No TURN server. Get one from computeengineondemand.appspot.com:
     var xhr = new XMLHttpRequest();
+    xhr.setRequestHeader('Access-Control-Allow-Origin','*');
     xhr.onreadystatechange = function(){
       if (xhr.readyState === 4 && xhr.status === 200) {
         var turnServer = JSON.parse(xhr.responseText);
